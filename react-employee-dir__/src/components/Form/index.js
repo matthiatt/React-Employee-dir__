95549fdc9e9dc186b6dc0c9f2b20d8264bd53a3d
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-20 01:42:56 -0600

    creating stuff.

---
+++ react-employee-dir__/src/components/Form/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Form extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    alert(`Hello there ${this.state.firstName} ${this.state.lastName}`);
+    this.setState({
+      firstName: "",
+      lastName: "",
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>
+          Hello {this.state.firstName} {this.state.lastName}
+        </p>
+        <form className="form">
+          <input
+            value={this.state.firstName}
+            name="firstName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            value={this.state.lastName}
+            name="lastName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Last Name"
+          />
+          <button onClick={this.handleFormSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
